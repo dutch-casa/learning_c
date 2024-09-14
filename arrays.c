562317fd6900e46d9b096eb38126a7b2a0f6860a
Dutch Casadaban <dutchcaz@Dutchs-MBP.lan>
2024-09-14 15:04:55 -0500

    arrays

---
+++ arrays.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    //use [] after the name, and {} to put objects in
+    int holyNumbers[] = {1, 3, 7, 12, 40, 555, 777};
+
+    //printf specific index in array
+    printf("%d", holyNumbers[1]);
+}
