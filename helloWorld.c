c40547473bdcffa4e889318ad620bb5d9e807991
Dutch Casadaban <dutchcaz@Dutchs-MBP.lan>
2024-09-14 14:15:37 -0500

    working with variables

--- helloWorld.c
+++ helloWorld.c
@@ -4,14 +4,27 @@
 int main() {
     printf("Hello, World!\n");
 
-    printf("       /\n");
-    printf("      / \\\n");
-    printf("     /   \\\n");
-    printf("    /     \\\n");
-    printf("   /       \\\n");
-    printf("  /         \\\n");
-    printf(" /___________\\\n");
+    printf("       /\\\n");
+    printf("      /  \\\n");
+    printf("     /    \\\n");
+    printf("    /      \\\n");
+    printf("   /        \\\n");
+    printf("  /          \\\n");
+    printf(" /____________\\\n");
 
+
+    // Let's experiment with some variables.
+
+    // C doesn't have strings, so we wil use a char array
+    char name[] = "Dutch";
+    int age = 20;
+
+    printf("My name is %s and I am %d years old.\n", name, age);
+
+    char name2[] = "Dave Grohl";
+    age = 47;
+
+    printf("%s is %d years old\n", name2, age);
 
     return 0;
 }
