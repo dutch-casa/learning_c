4319836224da558a3b97c64c566fe1ef92ee5c46
Dutch Casadaban <dutchcaz@Dutchs-MBP.lan>
2024-09-14 15:41:35 -0500

    structs practice

---
+++ structs.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+struct Student {
+    char name[50];
+    char major[50];
+    int age;
+    double gpa;
+};
+
+void studentToString(struct Student student) {
+    printf("Name: %s\n", student.name);
+    printf("Age: %d\n", student.age);
+    printf("GPA: %f\n", student.gpa);
+    printf("Major: %s\n", student.major);
+}
+
+
+int main() {
+    struct Student students[] = {
+        {"Dutch Casadaban", "Computer Science", 20, 4.0},
+        {"John Doe", "History", 22, 3.5},
+        {"Jane Doe", "Mathematics", 28, 3.0}
+    };
+
+    for(int i = 0; i < sizeof(students) / sizeof(struct Student); i++) {
+        studentToString(students[i]);
+    }
+
+    return 0;
+}
+
